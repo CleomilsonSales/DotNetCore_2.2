06ec2173e489e9f4bab004677eacf87351166870
Cleomilson Sales <cleomilsonsales@hotmail.com>
2021-10-08 11:55:38 -0300

    estudos de sexta 08.10.21 11:55

--- Angular/ProAgil-app/src/app/app.module.ts
+++ Angular/ProAgil-app/src/app/app.module.ts
@@ -1,26 +1,72 @@
-import { NgModule } from '@angular/core';
+// Módulos
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import {HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule, BsDropdownModule, ModalModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { ToastrModule } from 'ngx-toastr';
+
+// serviços
+import { EventoService } from './_services/evento.service';
+
+// Componentes
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import { TituloComponent } from './_shared/titulo/titulo.component';
+
+// Pipes
+import { DateFormatPipePipe } from './_helps/DateFormatPipe.pipe';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+
+
 
 @NgModule({
-  declarations: [		
-    AppComponent,
+   declarations: [
+      AppComponent,
+      NavComponent,
       EventosComponent,
-      NavComponent
+      PalestrantesComponent,
+      DashboardComponent,
+      ContatosComponent,
+      TituloComponent,
+      UserComponent,
+      LoginComponent,
+      RegistrationComponent,
+      DateFormatPipePipe
+
    ],
-  imports: [
+   imports: [
     BrowserModule,
+    BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+       timeOut: 3000,
+       preventDuplicates: true,
+       progressBar: true
+    }),
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    FormsModule,
+    ReactiveFormsModule
+ ],
+ providers: [
+    EventoService
+ ],
+ bootstrap: [
+    AppComponent
+ ]
 })
 export class AppModule { }
--- Angular/ProAgil-app/src/app/nav/nav.component.ts
+++ Angular/ProAgil-app/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+  }
+
+  loggedIn() {
+    // direciono para a função loggedIn criado '/_services/auth.service' porque lá ele já realiza a verificação de login
+    return this.authService.loggedIn();
+  }
+
+  entrar() {
+    this.router.navigate(['/user/login']);
+  }
+
+  logout() {
+    // aqui removo o token armazenado no local storange
+    localStorage.removeItem('token');
+    this.toastr.show('Log out efetuado!');
+    this.router.navigate(['/user/login']);
+
   }
 
 }
